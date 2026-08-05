3d14c670bb68353fcdb60df50ff769e7019d97dd
unknown <darklorderic16@gmail.com>
2026-08-05 22:36:01 +0200

    Import Fix

--- check_cves.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('graph.json', 'utf8'));
-const vulns = data.nodes.filter(n => (n.labels||[]).includes('Vulnerability') || n.primaryLabel === 'Vulnerability');
-vulns.forEach(v => console.log(v.properties.cve_id, Object.keys(v.properties)));
